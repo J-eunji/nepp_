31bc891b7a2f1173b4b65b20b21e6f6568ea246f
J-eunji <sun220711@gmail.com>
2022-09-02 14:31:31 +0900

    font적용

--- jej-portfolio/src/App.js
+++ jej-portfolio/src/App.js
@@ -2,9 +2,6 @@ import { createGlobalStyle } from "styled-components";
 import { RecoilRoot } from "recoil";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import scdream from "./static/fonts/SCDream3.ttf";
-import scdreamBold from "./static/fonts/SCDream7.ttf";
-import english from "./static/fonts/THEBOLDFONT.ttf";
 import "./font.css";
 
 function App() {
@@ -26,22 +23,17 @@ const GlobalStyle = createGlobalStyle`
   text-decoration: none;
 }
 
-#root p, span {
+:root {
+  p, span {
   font-family: 'content';
-}
-
-#root h4{
+  }
+  h4 {
   font-family: 'contentBold';
-}
-
-#root header, h1, h2, h3, h5, h6 {
+  }
+  header, h1, h2, h3, h5, h6 {
   font-family: 'titleEn';
   font-weight: lighter;
-}
-
-#root h1 {
-  font-family: 'titleEn';
-  font-weight: lighter;
+  }
 }
 `;
 
